ab3bb1b32c9a0eabe7e70b258c388eaae10d3d81
Immaculé Kayinda <immakaymw@gmail.com>
2024-02-14 12:13:22 +0100

    edit projet.css

--- src/projets.js
+++ src/projets.js
@@ -1,19 +1,19 @@
 const projets  = [{
-    title: "Bonjour",
-    description: "lorem Ipsum",
-    imageUrl: "https://github.com/immakaymw/immakaymw.github.io/blob/master/images/github.png?raw=true",
-    link: "https://github.com/immakaymw/immakaymw.github.io"
+    title: "Unity Care Project",
+    description: `UnityCare is a dynamic web and mobile platform that fosters unity and empowerment within communities by facilitating seamless connections between individuals in need of assistance and compassionate volunteers. The platform revolutionizes the way communities support one another`,
+    imageUrl: "./assets/andrea-piacquadio-3791664.jpg",
+    link: "https://github.com/UnityCareProject",
 }, {
     title: "Bonjour",
     description: "lorem Ipsum",
-    imageUrl: "https://github.com/immakaymw/immakaymw.github.io/blob/master/images/github.png?raw=true",
+    imageUrl: "https://www.natura-sciences.com/wp-content/uploads/2022/06/des-ordures-et-des-hommes-france-2.jpg",
     link: "https://github.com/immakaymw/immakaymw.github.io"
 }
 ]
 
 const projetListSection = document.querySelector("main");
 
-console.log(projetListSection)
+console.log(projetListSection)	
 
 /**
  * Creates an HTML element for a project item.
@@ -23,13 +23,17 @@ console.log(projetListSection)
 function createProjectItem(projet) {
   return `
     <li class="main__project-item">
-      <a href="${projet.link}" target="_blank" rel="noopener noreferrer">
-        <article class="main__project">
-          <h3 class="main__project-title">${projet.title}</h3>
-          <div class="main__project-description">${projet.description}</div>
-          <img src="${projet.imageUrl}" alt="Projet ${projet.title}" />
+			<article class="main__project">
+			<div class="main__project-description">
+			<h3 class="main__project-title">${projet.title}</h3>
+			<div class="main__project-text">${projet.description}</div>
+			</div>
+			<a href="${projet.link}" target="_blank" rel="noopener noreferrer">
+				<div class="main__project-picture">
+					<img src="${projet.imageUrl}" alt="Projet ${projet.title}" />
+				</div>
+			</a>
         </article>
-      </a>
     </li>
   `;
 }
